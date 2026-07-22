b7cac46146567b0e2e7089a8a3e5ac45ed44e797
Battoun <baptiste.canva@etu.univ-littoral.fr>
2026-07-21 23:27:38 -0700

    fix error page home

--- src/components/PhotoUpload.tsx
+++ src/components/PhotoUpload.tsx
@@ -118,7 +118,6 @@ export default function PhotoUpload({
             ref={inputRef}
             type="file"
             accept="image/*"
-            capture="environment"
             className="hidden"
             onChange={(e) => {
               const file = e.target.files?.[0];
